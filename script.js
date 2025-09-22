61b76e10f8195ff909dcbd21cf6a4eebbe45efe8
Rikke Rasmussen <RiMaRa@outlook.dk>
2025-09-22 15:55:17 +0200

    opsat lidt html og js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+let tekst = document.getElementById("tekstfelt"); // henter tekstarea
+let output = document.getElementById("output");
+
+// så input bliver opdateret hver gang brugeren skriver noget
+tekst.addEventListener("input", ordtaelling);
+
+function ordtaelling() {
+  let indhold = tekst.value; // henter brugerens tekst
+  let tegnMedMellumrum = indhold.length;
+  let tegnUdenMellemrum = indhold.replace(/\s+/g, "").length; //fjerner alle mellemrum i træk, så \s er ting som mellemrum, tab eller linjeskift. + er en eller flere i træk og g er global
+}
