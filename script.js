304c00d1268999ec1ef9b7745c8d9d5db67d6bd7
Rikke Rasmussen <RiMaRa@outlook.dk>
2025-09-23 15:50:55 +0200

    WIP med for løkken

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
-let tekst = document.getElementById("tekstfelt"); // henter tekstarea
-let output = document.getElementById("output");
+const tekst = document.getElementById("tekstfelt"); // henter tekstarea
+const output = document.getElementById("output");
 
 // så input bliver opdateret hver gang brugeren skriver noget
 tekst.addEventListener("input", ordtaelling);
@@ -14,10 +14,18 @@ function ordtaelling() {
   console.log("tegn uden mellemrum", tegnUdenMellemrum);
 
   // Tæller ordene op og tjekker om der er noget i det array som regX laver
-  let ordArray = indhold.match(/\S+/g); // \S er alle ikke whitespace tegn, så ord, tal og tegn
-  let antalOrd = ordArray ? ordArray.length : 0; // hvis der er ord, gemmes længden ellers er det lig 0
+  let ordArray = indhold.split(/\s+/g); // bruger split så teksten bliver til et array mellem whitespace
+  let ordUdenSkrifttegn = [];
 
-  console.log(antalOrd);
+  for (let i = 0; i < ordArray.length; ++i) {
+    let ord = ordArray[i].replace(/^[^\w]+|[^\w]+$/g, ""); // fjerner skrifttegnene fra starten og slutningen af ordet, og gør at : ikke tælles med hvis den står enkeltvist
+    if (ord.length > 0) {
+      ordUdenSkrifttegn.push(ord); // hvis ordet er større end 0 så tilføjes det i et array
+    }
+  }
+  let antalOrd = ordUdenSkrifttegn.length;
+
+  console.log(ordUdenSkrifttegn, antalOrd);
 
   // have tingene igennem output, måske i tre seperate dele fremfor i en fælles
   // sætte et maks antal på hvor mange tegn der må være?
