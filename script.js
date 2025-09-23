8c74629e18261b0dde41c105089a61b83158da93
Rikke Rasmussen <RiMaRa@outlook.dk>
2025-09-23 17:21:57 +0200

    rettet kommentarer og udkommenteret cl

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const tekst = document.getElementById("tekstfelt"); // henter tekstarea
 const output = document.getElementById("output");
 
-// så input bliver opdateret hver gang brugeren skriver noget
+// Opdatere ordtaelling hver gang der er et nyt input
 tekst.addEventListener("input", ordtaelling);
 
 function ordtaelling() {
@@ -10,33 +10,35 @@ function ordtaelling() {
   const tegnMedMellumrum = indhold.length;
   const tegnUdenMellemrum = indhold.replace(/\s+/g, "").length; //fjerner alle whitespaces. + er en eller flere i træk og g er global (regX)
 
-  console.log("Tegn med mellemrum", tegnMedMellumrum);
-  console.log("tegn uden mellemrum", tegnUdenMellemrum);
+  // console.log("Tegn med mellemrum", tegnMedMellumrum);
+  // console.log("tegn uden mellemrum", tegnUdenMellemrum);
 
   // Tæller ordene op og tjekker om der er noget i det array som regX laver
   const ordArray = indhold.split(/\s+/g); // bruger split så teksten bliver til et array mellem whitespace
   const ordUdenSkrifttegn = [];
 
+  // For løkken fjerne skrifttegnene fra starten af slutningen af ordet og gør at tegn som : ikke tælles med og godtager æøå
   for (let i = 0; i < ordArray.length; ++i) {
     let ord = ordArray[i].replace(
       /^[^a-zA-ZæøåÆØÅ\d]+|[^a-zA-ZæøåÆØÅ\d]+$/g,
       ""
-    ); // fjerner skrifttegnene fra starten og slutningen af ordet, og gør at : ikke tælles med hvis den står enkeltvist
+    );
     if (ord.length > 0) {
       ordUdenSkrifttegn.push(ord); // hvis ordet er større end 0 så tilføjes det i et array
     }
   }
   const antalOrd = ordUdenSkrifttegn.length;
 
-  // gemmesnit af tegn i ord
+  // Gemmesnit af tegn i ord
   let antalBogstver = 0;
+
+  // løkken gennemgår hvert ord i arrayet og lægger længden af hvert ord til
   for (let i = 0; i < ordUdenSkrifttegn.length; ++i) {
-    // løkken gennemgår hvert ord i arrayet og lægger længden af hvert ord til
     antalBogstver += ordUdenSkrifttegn[i].length;
   }
-  const gennemsnit = antalOrd > 0 ? (antalBogstver / antalOrd).toFixed(1) : 0; // hvis der er min. et år, deler den det totale antal af bogstager med antallet af ord. toFixed(giver et decimal)
+  const gennemsnit = antalOrd > 0 ? (antalBogstver / antalOrd).toFixed(1) : 0; // hvis der er min. et ord, deler den det totale antal af bogstager med antallet af ord. toFixed(giver et decimal)
 
-  console.log(ordUdenSkrifttegn, antalOrd, gennemsnit);
+  // console.log(ordUdenSkrifttegn, antalOrd, gennemsnit);
 
   // have tingene igennem output, måske i tre seperate dele fremfor i en fælles
   // sætte et maks antal på hvor mange tegn der må være?
